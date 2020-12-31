753a85c2beaf7a464bbd8e91864801c6d54411ff
ky <y.keddaoui@gmail.com>
2020-12-31 10:18:45 +0100

    fixes

--- src/services.js
+++ src/services.js
@@ -5,11 +5,10 @@ export async function recognizeSpeech(audioData, currentWord) {
     body: JSON.stringify({ audioStream: audioData, textToCheck: currentWord }),
   }
 
-  const response  = await fetch(
+  const response = await fetch(
     `https://api.staging.lxpia.com/api/users/speechToText`,
     requestOptions
   )
   const data = await response.json()
   return data
-    
 }
